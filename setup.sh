3dc6c7e3f440579b0116bd3275542d0907740c1f
Sridhara Dasu <dasu@hep.wisc.edu>
2022-03-01 14:01:47 -0600

    Fixed to work on both CentOS7 and CentOS8

--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
 #/bin/bash
-if [ -d '/cvmfs' ]; then source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos8-gcc11-opt/setup.sh; fi
+if [ -d '/cvmfs' ]; then
+    if [[ `uname -r` == *"el7"* ]]; then
+        source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
+        if [ -d "/nfs_scratch/$USER/CentOS7/MG5_aMC_v3_2_0" ]; then
+            export mg5dir=/nfs_scratch/$USER/CentOS7/MG5_aMC_v3_2_0/
+	fi
+    elif [[ `uname -r` == *"el8"* ]]; then
+        source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos8-gcc11-opt/setup.sh
+        if [ -d "/nfs_scratch/$USER/CentOS8/MG5_aMC_v3_2_0" ]; then
+            export mg5dir=/nfs_scratch/$USER/CentOS8/MG5_aMC_v3_2_0/
+	fi
+    fi
+fi
 if [ -d "$ROOTSYS" ]; then
     echo "Using ROOT from $ROOTSYS";
 else
     echo "ROOT is needed for this to work";
 fi
+
 if [ -d "$mg5dir" ]; then 
     echo "Using Madgraph5 from $mg5dir";
-elif [ -d "/nfs_scratch/$USER/MG5_aMC_v3_2_0" ]; then
-    export mg5dir=/nfs_scratch/$USER/MG5_aMC_v3_2_0/
-    echo "Setup for Madgraph5 from $mg5dir";
-elif [ -d "$HOME/MG5_aMC_v3_2_0" ]; then
-    export mg5dir=$/MG5_aMC_v3_2_0/
-    echo "Setup for Madgraph5 from $mg5dir";
 else
     echo "Madgraph is needed for this to work - export mg5dir=... and rerun";
 fi
